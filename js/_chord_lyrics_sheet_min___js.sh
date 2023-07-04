d8033d416658315668175913de8747bda32720e3
dev-hobbyist <dev.hobbyist.777@gmail.com>
2023-07-04 21:46:52 +0900

    youtube volume control

--- js/_chord_lyrics_sheet_min___js.sh
+++ js/_chord_lyrics_sheet_min___js.sh
@@ -1,13 +1,13 @@
 << comment
-<script type="text/javascript" src="js/ChordDB.js"></script>
-<script type="text/javascript" src="js/ChordManager.js"></script>
-<script type="text/javascript" src="js/Drums.js"></script>
-<script type="text/javascript" src="js/MusicXMLPlayer.js"></script>
-<script type="text/javascript" src="js/const.js"></script>
-<script type="text/javascript" src="js/youtube_iframe_player.js"></script>
-<script type="text/javascript" src="js/mango_player.js"></script>
-<script type="text/javascript" src="js/metronome.js"></script>
-<script type="text/javascript" src="js/chord_lyrics_sheet_control.js"></script>
+<script type="text/javascript" src="/js/ChordDB.js"></script>
+<script type="text/javascript" src="/js/ChordManager.js"></script>
+<script type="text/javascript" src="/js/Drums.js"></script>
+<script type="text/javascript" src="/js/MusicXMLPlayer.js"></script>
+<script type="text/javascript" src="/js/const.js"></script>
+<script type="text/javascript" src="/js/youtube_iframe_player.js"></script>
+<script type="text/javascript" src="/js/mango_player.js"></script>
+<script type="text/javascript" src="/js/metronome.js"></script>
+<script type="text/javascript" src="/js/chord_lyrics_sheet_control.js"></script>
 comment
 
 uglifyjs --compress --mangle -o chord_lyrics_sheet.min.js -- \
